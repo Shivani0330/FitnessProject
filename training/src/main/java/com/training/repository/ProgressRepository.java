d5a5a32d133949a9ee1fafb47c1b4d5dd15a440a
Shivani0330 <77954619+Shivani0330@users.noreply.github.com>
2024-01-21 19:05:23 +0530

    Delete training directory

--- training/src/main/java/com/training/repository/ProgressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.training.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.training.bean.Progress;
-
-public interface ProgressRepository extends JpaRepository<Progress, Long>{
-
-}
--- training/src/main/java/com/training/repository/WorkoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.training.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.training.bean.Workout;
-
-public interface WorkoutRepository extends JpaRepository<Workout, Long>{
-
-}
